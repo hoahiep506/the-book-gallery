f2d66707c2bb50c11bd7b98b92f56174b72b7b88
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-01-18 00:07:15 +0700

    lint card

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,33 +1,6 @@
 import data from '../../data/data'
 import style from './style.css'
 
-/*
-const Card = () => (
-  <div class={style.container}>
-    <div class={style.card}>
-      <div class={style.card__img}>
-        <img src='https://i.imgur.com/WesSXRt.jpg'></img>
-      </div>
-      <div class={style.info}>
-        <div class={style.info__title}>Get Ready for IELTS Writing</div>
-        <div class={style.info__author}>
-          <span>Fiona Aish</span>
-        </div>
-      </div>
-      <div class={style.btn}>
-        <a
-          href='https://shon.xyz/eMkA16'
-          target='_blank'
-          rel='noreferrer'
-          download
-        >
-          Download
-        </a>
-      </div>
-    </div>
-  </div>
-)
-*/
 
 const Card = () => (
   <div class={style.container}>
@@ -45,12 +18,7 @@ const Card = () => (
         </div>
 
         <div class={style.btn}>
-          <a
-            href={d.linkDl}
-            target='_blank'
-            rel='noreferrer'
-            download
-          >
+          <a href={d.linkDl} target='_blank' rel='noreferrer' download>
             Download
           </a>
         </div>

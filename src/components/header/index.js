b760b8ec7b08463dedf8d6f3dc7ace7ab7b57732
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-01-18 21:17:10 +0700

    add media query

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import style from './style.css'
 const Header = () => (
   <header class={style.header}>
     <h1 class={style.h1}> The Book Gallery</h1>
-    <p class={style.p}> Sharing IELTS &#x000B7; TOEIC books</p>
+    <p class={style.p}> Tài liệu IELTS &#x000B7; TOEIC miễn phí 🤩</p>
   </header>
 )
 export default Header

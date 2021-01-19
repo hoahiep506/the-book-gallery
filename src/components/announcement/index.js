d6783915f7fedc8b89d3c7e8f3a773e8d0857f84
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-01-19 16:05:11 +0700

    basic static announcement

--- src/components/announcement/index.js
+++ src/components/announcement/index.js
@@ -1,16 +1,36 @@
+import { h } from 'preact'
+import { useState } from 'preact/hooks'
 import style from './style.css'
 
+
+const createMarkup = () => ({
+  __html: '⭐️ Nếu bạn 💗️ thichtienganh, góp ý phát triển',
+})
+
+const createLink = () => 'https://www.youtube.com/watch?v=tl1hBnX-Gr4'
+
+
 const Announcement = () => (
+  
   <div class={style.container} role='banner'>
-    <span class={style.content}>
-      ⭐️ Giúp thichtienganh phát triển website bằng góp ý, đóng góp{' '}
-      <a class={style.link} rel='nofollow' href='#'>
-        {' '}
-        tại đây
-      </a>{' '}
-      ⭐️
-    </span>
+    <span
+      class={style.content}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={createMarkup()}
+    />
+    <a class={style.link} href={createLink()} target='_blank' rel='noreferrer'>
+      {' '}
+      tại đây
+    </a>{' '}
+    <span class={style.content}>⭐️</span>
+    <button aria-label='Close' class={style.button} type="button">
+      {' '}
+      <span aria-hidden='true' class={style.button__icon}>
+        ×
+      </span>
+    </button>
   </div>
 )
+
 
 export default Announcement

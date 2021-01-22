c2d30d4262e689ac72b4d456813077e3b3e18136
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-01-22 16:56:02 +0700

    add pagination function

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,6 +1,10 @@
+import { h } from 'preact'
+import usePagination from '@mariosant/react-pagination-hook'
+
 import data from '../../data/data'
 import style from './style.css'
 
+/*
 const Card = () => (
   <div class={style.container}>
     {data.map((d) => (
@@ -29,4 +33,50 @@ const Card = () => (
     ))}
   </div>
 )
+*/
+
+const Card = () => {
+  const { nextPage, previousPage, paginated } = usePagination(data)
+
+  return (
+    <div class={style.container}>
+      <div class={style.containerCard}>
+        {paginated.map((d) => (
+          <div class={style.card}>
+            <div class={style.card__img}>
+              <img src={d.img} alt={d.title} />
+            </div>
+
+            <div class={style.label}>
+              <span>{d.category}</span>
+            </div>
+
+            <div class={style.info}>
+              <div class={style.info__title}>{d.title}</div>
+              <div class={style.info__author}>
+                <span>{d.author}</span>
+              </div>
+            </div>
+
+            <div class={style.btn}>
+              <a href={d.linkDl} target='_blank' rel='noreferrer' download>
+                Download
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div class={style.buttonPage}>
+        <button class={style.buttonPage__child} onClick={previousPage}>
+          Quay lại trang trước
+        </button>
+        <button class={style.buttonPage__child} onClick={nextPage}>
+          Trang tiếp theo
+        </button>
+      </div>
+    </div>
+  )
+}
+
 export default Card
